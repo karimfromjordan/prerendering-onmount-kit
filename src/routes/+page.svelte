97e85d93b2687cecda3fb2c15bddca19c61fe306
Karim Jordan <karim@winter-rose.design>
2023-03-20 18:32:05 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="/articles/abc">abc</a>
+<a href="/articles/xyz">xyz</a>
+<a href="/verify?token=63we54d32w">verify</a>
---
+++ src/routes/verify/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let token = '';
+
+	onMount(() => {
+		token = new URLSearchParams(window.location.search).get('token');
+	});
+</script>
+
+<h1>{token}</h1>
